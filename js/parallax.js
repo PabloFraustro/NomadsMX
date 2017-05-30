61d5eb061c378d24a15171bad7915e7eb019dac7
Pablo Fraustro <pablofraustro@gmail.com>
2017-05-29 21:21:32 -0500

    Arreglé el problema del aside bar, cambie algunos elementos para que sean web-reponsive, faltan historias y cambiar algunas configuraciones para telefonos móviles

--- js/parallax.js
+++ js/parallax.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$(window).scroll(function(){
 		var windowWidth = $(window).width();
 
-		if(windowWidth > 800){
+		if(windowWidth > 720){
 			var scroll = $(window).scrollTop();
 
 			$('#about-article').css({
@@ -11,7 +11,7 @@ $(document).ready(function(){
 			});
 
       $('#about-photo').css({
-				'transform': 'translate(0px,-' + scroll/4.5 + '%)'
+				'transform': 'translate(0px,-' + scroll/1.5 + '%)'
 			});
 
 			$('#title').css({
