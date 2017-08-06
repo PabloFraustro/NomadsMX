4f580922d92abf353b694a0682f544b8b7ffabbc
Pablo Fraustro <pablofraustro@gmail.com>
2017-08-06 16:14:52 -0500

    Menu incluido, fontawesome actualizado y funcional, todo bien

--- js/animations.js
+++ js/animations.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
    $(window).scrollTop(0);
 
-  // Title effect
+  // Entrance effects
   $('#title').animate({
     'top': '60px',
     'opacity' : '1'
@@ -12,6 +12,22 @@ $('#about').animate({
     'right' :'0',
     'opacity' : '1'
 },1000);
+
+$('#menu').animate({
+    'left' :'0',
+    'opacity' : '1'
+},1000);
+
+// Scroll down animation before scrolling
+
+$(window).on("scroll",function(){
+  if($(window).scrollTop() > 0){
+    $('#arrows').css({'visibility':'hidden'})
+  }
+  else{
+    $('#arrows').css({'visibility':'visible'})
+  }
+})
 
 
 });
